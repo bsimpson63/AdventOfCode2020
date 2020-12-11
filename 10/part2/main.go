d96c154747107be623df714114edceaf0dceaf8c
Brian Simpson <bsimpson63@gmail.com>
2020-12-10 16:06:52 -0800

    memoized solution to day 10 part 2

--- 10/part2/main.go
+++ 10/part2/main.go
@@ -8,7 +8,14 @@ import (
 	"strconv"
 )
 
+var countFromPoint = make(map[int]int)
+
 func findTerminationCount(adaptorSet map[int]bool, start int, end int) int {
+	memoizedCount, ok := countFromPoint[start]
+	if ok {
+		return memoizedCount
+	}
+
 	if start == end {
 		return 1
 	}
@@ -24,52 +31,8 @@ func findTerminationCount(adaptorSet map[int]bool, start int, end int) int {
 			count += findTerminationCount(adaptorSet, needed, end)
 		}
 	}
+	countFromPoint[start] = count
 	return count
-}
-
-func findsChokePoints(adaptors []int) []int {
-	/*
-		a point is a choke if all paths must go through it
-		all paths must go through a point if the
-		previous point can only go to the current point
-	*/
-	adaptorSet := make(map[int]bool)
-	for _, adaptor := range adaptors {
-		adaptorSet[adaptor] = true
-	}
-
-	chokePoints := make([]int, 0)
-
-	// is 0 a choke point?
-	optionCount := 0
-	for offset := 1; offset <= 3; offset++ {
-		needed := 0 + offset
-		if _, ok := adaptorSet[needed]; ok {
-			optionCount++
-		}
-	}
-	if optionCount == 1 {
-		chokePoints = append(chokePoints, 0)
-	}
-
-	for i, adaptor := range adaptors {
-		if i == 0 {
-			// already considered this above
-			continue
-		}
-		previous := adaptors[i-1]
-		optionCount := 0
-		for offset := 1; offset <= 3; offset++ {
-			needed := previous + offset
-			if _, ok := adaptorSet[needed]; ok {
-				optionCount++
-			}
-		}
-		if optionCount == 1 {
-			chokePoints = append(chokePoints, adaptor)
-		}
-	}
-	return chokePoints
 }
 
 func main() {
@@ -102,42 +65,5 @@ func main() {
 	for _, adaptor := range adaptors {
 		adaptorSet[adaptor] = true
 	}
-
-	chokePoints := findsChokePoints(adaptors)
-	chokePointsSet := make(map[int]bool)
-	for _, chokePoint := range chokePoints {
-		chokePointsSet[chokePoint] = true
-	}
-
-	fmt.Println("choke points at", chokePoints)
-
-	isChoked := true
-	lastChoke := 0
-	paths := 1
-	for _, adaptor := range adaptors {
-		_, isChokePoint := chokePointsSet[adaptor]
-
-		if !isChokePoint {
-			if isChoked {
-				// transitioning from choked to open
-				isChoked = false
-
-			} else {
-				// still unchoked
-			}
-		} else {
-			if isChoked {
-				// still choked
-				lastChoke = adaptor
-
-			} else {
-				// transitioning from open to choked
-				fmt.Printf("found gap %d->%d\n", lastChoke, adaptor)
-				isChoked = true
-				paths *= findTerminationCount(adaptorSet, lastChoke, adaptor)
-			}
-
-		}
-	}
-	fmt.Println("paths", paths)
+	fmt.Println(findTerminationCount(adaptorSet, 0, adaptors[len(adaptors)-1]))
 }
